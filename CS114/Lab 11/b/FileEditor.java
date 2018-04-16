63abde9660316e3a9d6cb3b9bfceeb5acd899072
Matt <Matt@DESKTOP-7B1H486.resource.campus.njit.edu>
2018-04-16 00:22:11 -0400

    fin lab 11

--- CS114/Lab 11/b/FileEditor.java
+++ CS114/Lab 11/b/FileEditor.java
@@ -6,103 +6,83 @@ import java.io.*;
 import java.util.Scanner;
 import javax.swing.*;
 
-public class FileEditor {
-	public static void main(String[] args) {
-		 new FileProgram();
+public class FileEditor{
+
+	public static void main(String[] args) throws FileNotFoundException{
+		new FileProgram();
 	}
 }
 
-class FileProgram extends JFrame {
+class FileProgram extends JFrame{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private File file;
+	private String contents = "";
+	private JButton save = new JButton("Save & Close");
+	private JButton close = new JButton("Exit ");
+	private JFrame frame = new JFrame("hi kevin");
+	private JTextArea textArea = new JTextArea();
 
-	public FileProgram() {
+	public FileProgram() throws FileNotFoundException{
 		readFile();
 		fileEditor();
 	}
 
-	private File selectedFile;
-	private String selectedFileContents = "";
-	private JButton SaveAndExit = new JButton("Save & Close");
-	private JButton Exit = new JButton("Exit ");
-	private JFrame frame = new JFrame("hi kevin");
-	private JTextArea textArea = new JTextArea();
-	
-	
+	private void readFile() throws FileNotFoundException{
+		String path = System.getProperty("user.dir") + "\\Lab 11\\b";
+		JFileChooser fileChooser = new JFileChooser(path);
 
-	private void readFile() {
-		try {
-			//default location
-			String path = System.getProperty("user.dir") + "\\Part 2";
-			JFileChooser aFileChooser = new JFileChooser(path);
-			
-			int returnValue = aFileChooser.showOpenDialog(null);
+		int returnVal = fileChooser.showOpenDialog(null);
 
-			if (returnValue == JFileChooser.APPROVE_OPTION) {
-				selectedFile = aFileChooser.getSelectedFile();
-				System.out.println(selectedFile.getAbsolutePath());
-				Scanner scan = new Scanner(selectedFile);
-				
-				//get contents of file
-				while (scan.hasNextLine()) {
-					String temp = scan.nextLine() + "\n";
-					selectedFileContents = selectedFileContents + temp;
-				}
-				scan.close();
-				textArea.setText(selectedFileContents); //sets text editor contents to that of file
+		if (returnVal == JFileChooser.APPROVE_OPTION){
+			file = fileChooser.getSelectedFile();
+			System.out.println(file.getAbsolutePath());
+			Scanner scan = new Scanner(file);
+
+			while (scan.hasNextLine()){
+				contents = contents + scan.nextLine() + "\n";
 			}
-			
-		} catch (Exception exception) {
-			exception.printStackTrace();
+			textArea.setText(contents);
 		}
+
 	}
 
-	private void fileEditor() {
+	private void fileEditor(){
 		JPanel textField = new JPanel(new GridLayout(1, 1));
 		JPanel buttons = new JPanel(new GridLayout(1, 2));
 		textField.add(textArea);
 		frame.add(textField);
 		frame.getContentPane().add(buttons, BorderLayout.SOUTH);
-		buttons.add(SaveAndExit);
-		buttons.add(Exit);
+		buttons.add(save);
 		
-		//Listener for Close
-		Exit.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frame.dispose();
+		save.addActionListener(				
+			new ActionListener(){
+				public void actionPerformed(ActionEvent e){
+					try{
+						save();
+					}
+					catch (IOException e1){
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					frame.dispose();
+				}
 			}
-		});
-		
-		
-		//Listener for Save
-		SaveAndExit.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				save();
-				frame.dispose();
-			}
-		});
-		
-		frame.setSize(69*10, 420);
+		);
+
+		frame.setSize(1820, 1340);
+		setBackground(Color.GREEN);
 		frame.setVisible(true);
-		frame.setLocationRelativeTo(null);
 	}
 
-	private void save() {
-		try {
-			String path = selectedFile.getAbsolutePath();
-			//create a new file w/ same name as original
-			File newFile = new File(path.substring(path.lastIndexOf(",") + 1));
-			//write edited text to new file
-			FileWriter aWriter = new FileWriter(newFile);
-			aWriter.write(textArea.getText());
-			aWriter.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	private void save() throws IOException{
+		String path = file.getAbsolutePath();
+		File newFile = new File(path.substring(path.lastIndexOf(",") + 1));
+		FileWriter writer = new FileWriter(newFile);
+		writer.write(textArea.getText());
+		writer.close();
 	}
 }
